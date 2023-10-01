6bded613d4c64db719f3ffd9f0acc04fd338f04e
andre yonashiro <d3.opetechmydod@gmail.com>
2023-10-01 21:15:40 +0900

    finished gatsby tutorial part5!

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,18 @@
  */
 module.exports = {
   siteMetadata: {
-    title:'My first gatsby site',
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
   ],
-}
+};
